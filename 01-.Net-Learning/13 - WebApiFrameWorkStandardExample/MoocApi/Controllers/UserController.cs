5dbca5166d6dd92bf1788f76d19720d90e151542
Allen5288 <yuanxin_xie_ee@163.com>
2025-06-28 13:53:08 +1000

    feat: add a web api frameword standard project for sample

---
+++ 01-.Net-Learning/13 - WebApiFrameWorkStandardExample/MoocApi/Controllers/UserController.cs
@@ -0,0 +1,126 @@
+﻿using IMoocService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using MoocApi.MoocFilters;
+using MoocModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace MoocApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [TypeFilter(typeof(MoocActionFilter), Arguments = new object[] { true })]
+        [HttpGet()]
+        public ApiResult Get(int id)
+        {
+            var user = _userService.GetUserById(id);
+            if (user == null)
+                return new ApiResult { Success = false, Data = null, Message = "no users" };
+
+            else
+                return new ApiResult { Success = true, Data = user, Message = "success" };
+        }
+
+        [HttpPost]
+        public ApiResult Add(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<string> errorList = new List<string>();
+                foreach (var item in ModelState.Keys)
+                {
+                    if (ModelState[item].Errors != null && ModelState[item].Errors.Count > 0)
+                    {
+                        foreach (var error in ModelState[item].Errors)
+                        {
+
+                            errorList.Add(item + ":" + error.ErrorMessage);
+                        }
+                    }
+                }
+
+                return new ApiResult
+                {
+                    Success = false,
+                    Errors = errorList.ToArray(),
+                };
+
+            }
+
+            var isSuccess = _userService.AddUser(user);
+
+            return new ApiResult { Success = isSuccess, Data = user, Message = isSuccess ? "success" : "false" };
+        }
+
+        [HttpPut]
+        public ApiResult Update(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                List<string> errorList = new List<string>();
+                foreach (var item in ModelState.Keys)
+                {
+                    if (ModelState[item].Errors != null && ModelState[item].Errors.Count > 0)
+                    {
+                        foreach (var error in ModelState[item].Errors)
+                        {
+
+                            errorList.Add(item + ":" + error.ErrorMessage);
+                        }
+                    }
+                }
+
+                return new ApiResult
+                {
+                    Success = false,
+                    Errors = errorList.ToArray(),
+                };
+
+            }
+
+            var isSuccess = _userService.UpdateUser(user);
+
+            return new ApiResult { Success = isSuccess, Data = user, Message = isSuccess ? "success" : "false" };
+        }
+
+        [HttpDelete("{id}")]
+        public ApiResult Delete(int id)
+        {
+            var user = _userService.GetUserById(id);
+            if (user == null)
+                return new ApiResult { Success = false, Data = null, Message = "no users" };
+            else
+            {
+                var isSuccess = _userService.DeleteUser(id);
+                return new ApiResult { Success = true, Data = user, Message = "success" };
+            }
+
+        }
+
+        [HttpGet("Getall")]
+        public ApiResult GetAll()
+        {
+            //throw new NotImplementedException("this is for the global error handling");
+
+            return new ApiResult { Success = true, Data = null, Message = true ? "success" : "false" };
+          
+        }
+
+        [HttpGet("test")]
+        public IActionResult Test(int id)
+        {
+            return Ok(new { id });
+        }
+    }
+}
---
+++ 01-.Net-Learning/13 - WebApiFrameWorkStandardExample/MoocApi/MoocFilters/MoocResultFilter.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MoocModel;
+
+namespace MoocApi.MoocFilters
+{
+    public class MoocResultFilter : IResultFilter
+    {
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            // 在结果执行前处理
+            if (context.Result is ObjectResult objectResult)
+            {
+                // 如果已经是我们的统一格式，则添加时间戳
+                if (objectResult.Value is ApiResult apiResult)
+                {
+                    // 确保 Errors 不为 null
+                    apiResult.Errors ??= Array.Empty<object>();
+
+                    // 添加时间戳
+                    apiResult.Data = new
+                    {
+                        Timestamp = DateTime.UtcNow,
+                        Content = apiResult.Data
+                    };
+                }
+                else
+                {
+                    // 如果不是统一格式，转换为统一格式
+                    var newApiResult = new ApiResult
+                    {
+                        Success = true,
+                        Message = "Operation succeeded",
+                        Data = new
+                        {
+                            Timestamp = DateTime.UtcNow,
+                            Content = objectResult.Value
+                        },
+                        Errors = Array.Empty<object>()
+                    };
+
+                    objectResult.Value = newApiResult;
+                    objectResult.DeclaredType = typeof(ApiResult);
+                }
+            }
+
+        }
+    }
+}
